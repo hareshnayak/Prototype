7e23ae9f89e2130cbf52d25acea474745145e82e
HimeshNayak <Himeshnayak015@gmail.com>
2020-07-15 19:59:19 +0530

    Added Chat App functionality

--- lib/messsage.dart
+++ lib/messsage.dart
@@ -29,7 +29,7 @@ class _MessagesState extends State<Messages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _loadMessages(context),
+      body: Text('Messages'),
       bottomNavigationBar: TextField(
           decoration: InputDecoration(
             hintText: 'Type Message',
@@ -47,6 +47,43 @@ class _MessagesState extends State<Messages> {
     );
   }
 
-
+//  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+//    return ListView(
+//      padding: const EdgeInsets.only(top: 20.0),
+//      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+//    );
+//  }
+//
+//  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//    final record = Record.fromSnapshot(data);
+//
+//    return Padding(
+//      padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+//      child: SizedBox(
+//        child: Card(
+//          child: InkWell(
+//            splashColor: Colors.amber[300],
+//            onTap: () {},
+//            child: ListTile(
+//              trailing: FloatingActionButton(
+//                mini: true,
+//                backgroundColor: Colors.blueGrey[900],
+//                onPressed: () {
+//                  String url = record.link;
+//                  launch(url);
+//                },
+//                child: Icon(Icons.cloud_download, color: Colors.white),
+//              ),
+//              title: Text(record.heading),
+//              subtitle: Text(
+//                record.date,
+//                style: TextStyle(fontSize: 10.0),
+//              ),
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
 
 }
