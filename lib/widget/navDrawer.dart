a79b086114e18d3ca79e1b6e30ed5e90b5e54675
hareshnayak <61956975+hareshnayak@users.noreply.github.com>
2020-07-20 03:21:28 +0530

    Update navDrawer.dart

--- lib/widget/navDrawer.dart
+++ lib/widget/navDrawer.dart
@@ -14,47 +14,60 @@ class NavDrawer extends StatelessWidget {
               decoration: BoxDecoration(
                   image: DecorationImage(
                       fit: BoxFit.cover,
-                      image: NetworkImage('https://wallpapercave.com/wp/PvKBonY.jpg'))),
+                      image: NetworkImage(
+                          'https://wallpapercave.com/wp/PvKBonY.jpg'))),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Text('Haresh Nayak',
+                  Text(
+                    'Haresh Nayak',
                     style: TextStyle(color: Colors.white, fontSize: 25),
                   ),
-                ],),),
+                ],
+              ),
+            ),
             ListTile(
-              leading: Icon(Icons.book),
+              leading: Icon(Icons.book, color: Colors.deepPurple),
               title: Text('Courses'),
               onTap: () => {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SearchActivity()),)
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => SearchActivity()),)
               },
             ),
-            ListTile(
-              leading: Icon(Icons.book),
-              title: Text('Chat'),
-              onTap: () => {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen(title: 'Chat Box')),)
-              },
+            Container(
+              color: Colors.grey.shade300,
+              child: ListTile(
+                leading: Icon(Icons.chat, color: Colors.deepPurple),
+                title: Text('Chat'),
+                onTap: () => {
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen(title: 'Chat Box')),)
+                },
+              ),
             ),
             ListTile(
-              leading: Icon(Icons.verified_user),
+              leading: Icon(Icons.verified_user, color: Colors.deepPurple),
               title: Text('Profile'),
               onTap: () => {Navigator.of(context).pop()},
             ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () => {Navigator.of(context).pop()},
+            Container(
+              color: Colors.grey.shade300,
+              child: ListTile(
+                leading: Icon(Icons.settings, color: Colors.deepPurple),
+                title: Text('Settings'),
+                onTap: () => {Navigator.of(context).pop()},
+              ),
             ),
             ListTile(
-              leading: Icon(Icons.border_color),
+              leading: Icon(Icons.border_color, color: Colors.deepPurple),
               title: Text('Feedback'),
               onTap: () => {Navigator.of(context).pop()},
             ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Logout'),
-              onTap: () => {Navigator.of(context).pop()},
+            Container(
+              color: Colors.grey.shade300,
+              child: ListTile(
+                leading: Icon(Icons.exit_to_app, color: Colors.deepPurple),
+                title: Text('Logout'),
+                onTap: () => {Navigator.of(context).pop()},
+              ),
             ),
           ],
         ),
